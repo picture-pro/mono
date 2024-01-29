6837315ad1da99249630166ad09abf93ed3e244f
John Lewis <github@jlewis.sh>
2024-01-29 15:50:37 -0600

    added axum

--- engine/crates/engine/src/main.rs
+++ engine/crates/engine/src/main.rs
@@ -1,10 +1,15 @@
+use axum::{routing::get, Router};
 use color_eyre::eyre::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
   color_eyre::install()?;
 
-  println!("Hello, world!");
+  let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+  // run our app with hyper, listening globally on port 3000
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+  axum::serve(listener, app).await?;
 
   Ok(())
 }
