27e6357222142772e4a4f19fc4389768e149dac8
John Lewis <github@jlewis.sh>
2024-01-29 16:31:59 -0600

    extracted app to module

--- engine/crates/engine/src/main.rs
+++ engine/crates/engine/src/main.rs
@@ -1,6 +1,6 @@
-use axum::{routing::get, Router};
+pub mod app;
+
 use color_eyre::eyre::Result;
-use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -8,16 +8,8 @@ async fn main() -> Result<()> {
   let subscriber = tracing_subscriber::fmt().finish();
   tracing::subscriber::set_global_default(subscriber)?;
 
-  let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-
-  // run our app with hyper, listening globally on port 3000
-  let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
-  let host = std::env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
-  let address = format!("{}:{}", host, port);
-  let listener = tokio::net::TcpListener::bind(&address).await?;
-
-  info!("Listening on {}", address);
-  axum::serve(listener, app).await?;
+  let app = app::App::new();
+  app.serve().await?;
 
   Ok(())
 }
