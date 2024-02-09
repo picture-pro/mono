537100a9251f8824ca5463768383b4f791bebd9d
John Lewis <github@jlewis.sh>
2024-02-09 11:31:58 -0600

    fully integrate axum-login

--- crates/site-server/src/main.rs
+++ crates/site-server/src/main.rs
@@ -1,13 +1,62 @@
-use axum::{routing::post, Router};
+use axum::{
+  body::Body,
+  extract::{FromRef, Path, State},
+  http::Request,
+  response::{IntoResponse, Response},
+  routing::{get, post},
+  Router,
+};
 use color_eyre::eyre::Result;
 use fileserv::file_and_error_handler;
-use leptos::*;
-use leptos_axum::{generate_route_list, LeptosRoutes};
+use leptos::{logging::log, *};
+use leptos_axum::{
+  generate_route_list, handle_server_fns_with_context, LeptosRoutes,
+};
+use leptos_router::RouteListing;
 use site_app::*;
 use tower::ServiceBuilder;
 use tower_http::compression::CompressionLayer;
 
 pub mod fileserv;
+
+#[derive(FromRef, Debug, Clone)]
+pub struct AppState {
+  pub leptos_options: LeptosOptions,
+  pub routes:         Vec<RouteListing>,
+}
+
+async fn server_fn_handler(
+  State(app_state): State<AppState>,
+  auth_session: auth::AuthSession,
+  path: Path<String>,
+  request: Request<Body>,
+) -> impl IntoResponse {
+  log!("{:?}", path);
+
+  handle_server_fns_with_context(
+    move || {
+      provide_context(auth_session.clone());
+    },
+    request,
+  )
+  .await
+}
+
+async fn leptos_routes_handler(
+  auth_session: auth::AuthSession,
+  State(app_state): State<AppState>,
+  req: Request<Body>,
+) -> Response {
+  let handler = leptos_axum::render_route_with_context(
+    app_state.leptos_options.clone(),
+    app_state.routes.clone(),
+    move || {
+      provide_context(auth_session.clone());
+    },
+    site_app::App,
+  );
+  handler(req).await.into_response()
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -25,19 +74,26 @@ async fn main() -> Result<()> {
   let conf = get_configuration(None).await.unwrap();
   let leptos_options = conf.leptos_options;
   let addr = leptos_options.site_addr;
+  let state = AppState {
+    leptos_options: leptos_options.clone(),
+    routes:         generate_route_list(App),
+  };
   let routes = generate_route_list(App);
 
   // build our application with a route
   let app = Router::new()
-    .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
-    .leptos_routes(&leptos_options, routes, App)
+    .route(
+      "/api/*fn_name",
+      get(server_fn_handler).post(server_fn_handler),
+    )
+    .leptos_routes_with_handler(routes, get(leptos_routes_handler))
     .fallback(file_and_error_handler)
     .layer(
       ServiceBuilder::new()
         .layer(CompressionLayer::new())
         .layer(auth::build_auth_layer().await?),
     )
-    .with_state(leptos_options);
+    .with_state(state);
 
   // run our app with hyper
   // `axum::Server` is a re-export of `hyper::Server`
