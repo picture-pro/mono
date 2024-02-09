ddd3a20a4beba1af46da5e8fae35995bc0e11c6c
John Lewis <github@jlewis.sh>
2024-02-09 12:40:19 -0600

    fixed artifacts in nix output

--- flake.nix
+++ flake.nix
@@ -104,7 +104,7 @@
           '';
           installPhaseCommand = ''
             mkdir -p $out/bin
-            cp target/release/site-server $out/bin/site-server
+            cp target/release/site-server $out/bin/
             cp -r target/site $out/bin/
           '';
           # Prevent cargo test and nextest from duplicating tests
