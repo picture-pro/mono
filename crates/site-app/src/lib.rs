2e6a27e128fe6fe9126349ea969214c69f7f2fb9
John Lewis <github@jlewis.sh>
2024-02-12 11:52:26 -0600

    add navbar

--- crates/site-app/src/lib.rs
+++ crates/site-app/src/lib.rs
@@ -7,7 +7,10 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
-use crate::error_template::{AppError, ErrorTemplate};
+use crate::{
+  error_template::{AppError, ErrorTemplate},
+  utils::authenticated_user,
+};
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -26,13 +29,38 @@ pub fn App() -> impl IntoView {
       outside_errors.insert_with_default_key(AppError::NotFound);
       view! { <ErrorTemplate outside_errors/> }.into_view()
     }>
-      <main data-theme="business">
+      <div data-theme="corporate" class="w-full min-h-screen flex flex-col items-stretch justify-stretch">
+        <Navbar/>
         <Routes>
           <Route path="" view=pages::home_page::HomePage/>
           <Route path="/login" view=pages::auth::login_page::LoginPage/>
           <Route path="/signup" view=pages::auth::signup_page::SignupPage/>
         </Routes>
-      </main>
+      </div>
     </Router>
+  }
+}
+
+#[component]
+pub fn Navbar() -> impl IntoView {
+  let current_user = authenticated_user();
+  let home_url = if current_user.is_some() {
+    "/dashboard"
+  } else {
+    "/"
+  };
+
+  view! {
+    <div class="bg-base-200 w-full shadow-xl">
+      <div class="d-navbar container mx-auto">
+        <div class="flex-1">
+          <a class="d-btn d-btn-ghost text-xl" href={home_url}>PicturePro</a>
+        </div>
+        <div class="flex-none flex flex-row items-center gap-2">
+          <a class="d-btn d-btn-ghost" href="/login">Login</a>
+          <a class="d-btn d-btn-ghost" href="/signup">Signup</a>
+        </div>
+      </div>
+    </div>
   }
 }
