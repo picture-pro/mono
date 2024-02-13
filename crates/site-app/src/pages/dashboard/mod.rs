6947de10ed915ec5f4a2045a932f570fb89c4284
John Lewis <github@jlewis.sh>
2024-02-13 10:34:51 -0600

    added large page wrapper

--- crates/site-app/src/pages/dashboard/mod.rs
+++ crates/site-app/src/pages/dashboard/mod.rs
@@ -1,10 +1,12 @@
 use leptos::*;
 
+use crate::pages::PageWrapper;
+
 #[component]
 pub fn DashboardPage() -> impl IntoView {
   view! {
-    <div class="flex-1 flex flex-col md:container md:mx-auto mt-8 mb-0 md:my-8 md:rounded-xl shadow p-4 md:px-6 bg-base-200">
-      <p class="text-xl font-semibold tracking-tight">"Dashboard Page"</p>
-    </div>
+    <PageWrapper>
+      <p class="text-4xl font-semibold tracking-tight">"Dashboard Page"</p>
+    </PageWrapper>
   }
 }
--- crates/site-app/src/pages/mod.rs
+++ crates/site-app/src/pages/mod.rs
@@ -14,3 +14,12 @@ pub fn SmallPageWrapper(children: Children) -> impl IntoView {
     </div>
   }
 }
+
+#[component]
+pub fn PageWrapper(children: Children) -> impl IntoView {
+  view! {
+    <div class="flex-1 flex flex-col md:container md:mx-auto mt-8 mb-0 md:my-8 md:rounded-xl shadow p-4 md:px-6 bg-base-200">
+      {children()}
+    </div>
+  }
+}
