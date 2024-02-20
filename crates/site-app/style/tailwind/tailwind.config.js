e365c95981c2e73000831c249a49b5c9467802bb
John Lewis <github@jlewis.sh>
2024-02-20 12:02:59 -0600

    feat: switched to modified wireframe daisy theme

--- crates/site-app/style/tailwind/tailwind.config.js
+++ crates/site-app/style/tailwind/tailwind.config.js
@@ -14,7 +14,31 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "wireframe"],
+    themes: [
+      {
+        wireframe: {
+          "color-scheme": "light",
+          // "fontFamily": "Chalkboard,comic sans ms,'sans-serif'",
+          "primary": "#b8b8b8",
+          "secondary": "#b8b8b8",
+          "accent": "#b8b8b8",
+          "neutral": "#ebebeb",
+          "base-100": "oklch(100% 0 0)",
+          "base-200": "#eeeeee",
+          "base-300": "#dddddd",
+          "info": "#0000ff",
+          "success": "#008000",
+          "warning": "#a6a659",
+          "error": "#ff0000",
+          // "--rounded-box": "0.2rem",
+          // "--rounded-btn": "0.2rem",
+          // "--rounded-badge": "0.2rem",
+          // "--tab-radius": "0.2rem",
+        }
+      },
+      "light",
+      "dark",
+    ],
     prefix: "d-",
   },
 };
