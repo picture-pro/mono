b15ee9601368ce2050a14901bb7583fea8eb84fc
John Lewis <github@jlewis.sh>
2024-02-20 11:09:54 -0600

    feat: migrated photo thumb fetch to `bl`

--- crates/bl/src/fetch.rs
+++ crates/bl/src/fetch.rs
@@ -1,7 +1,10 @@
+use artifact::Artifact;
 use clients::surreal::SurrealRootClient;
-use color_eyre::eyre::{Context, Result};
+use color_eyre::eyre::{Context, OptionExt, Result};
 use core_types::{CoreId, PhotoGroup};
 use tracing::instrument;
+
+use crate::model_ext::ModelExt;
 
 #[instrument]
 pub async fn fetch_user_owned_photo_groups(
@@ -30,4 +33,47 @@ pub async fn fetch_user_owned_photo_groups(
     .wrap_err("Failed to take result of photo groups query")?;
 
   Ok(groups)
+}
+
+#[tracing::instrument]
+pub async fn fetch_photo_thumbnail(
+  photo_id: core_types::PhotoRecordId,
+) -> Result<core_types::PhotoThumbnailDisplayParams> {
+  // prep the surreal client
+  let client = clients::surreal::SurrealRootClient::new().await?;
+  client
+    .use_ns("main")
+    .use_db("main")
+    .await
+    .wrap_err("Failed to start surreal client")?;
+
+  let photo = core_types::Photo::fetch(photo_id, &client)
+    .await
+    .wrap_err("Failed to select photo")?
+    .ok_or_eyre("Failed to select photo: photo is missing")?;
+
+  // select the thumbnail artifact
+  let mut thumbnail_artifact: core_types::PublicArtifact = (*client)
+    .select(photo.artifacts.thumbnail.artifact_id)
+    .await
+    .wrap_err("Failed to select thumbnail artifact")?
+    .ok_or_eyre("Failed to select thumbnail artifact: artifact is missing")?;
+
+  // download the thumbnail artifact and get the content
+  thumbnail_artifact
+    .download()
+    .await
+    .wrap_err("Failed to download photo thumbnail artifact")?;
+  let thumbnail_artifact_content = thumbnail_artifact
+    .contents
+    .ok_or_eyre("Thumbnail artifact is missing contents")?;
+
+  use base64::prelude::*;
+  let data = BASE64_STANDARD.encode(&thumbnail_artifact_content);
+
+  Ok(core_types::PhotoThumbnailDisplayParams {
+    data,
+    alt: "Thumbnail".to_string(),
+    size: photo.artifacts.thumbnail.size,
+  })
 }
--- crates/site-app/src/components/photo.rs
+++ crates/site-app/src/components/photo.rs
@@ -1,12 +1,4 @@
 use leptos::*;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct PhotoThumbnailDisplayParams {
-  data: String,
-  alt:  String,
-  size: (u32, u32),
-}
 
 #[component]
 pub fn Photo(photo_id: core_types::PhotoRecordId) -> impl IntoView {
@@ -42,67 +34,12 @@ pub fn Photo(photo_id: core_types::PhotoRecordId) -> impl IntoView {
 #[server]
 pub async fn fetch_photo_thumbnail(
   photo_id: core_types::PhotoRecordId,
-) -> Result<PhotoThumbnailDisplayParams, ServerFnError> {
-  use artifact::Artifact;
-  use base64::prelude::*;
-  use tracing::{info_span, Instrument};
-
-  // prep the surreal client
-  let surreal_client = clients::surreal::SurrealRootClient::new()
-    .instrument(info_span!("create_surreal_client"))
+) -> Result<core_types::PhotoThumbnailDisplayParams, ServerFnError> {
+  bl::fetch::fetch_photo_thumbnail(photo_id)
     .await
     .map_err(|e| {
-      ServerFnError::new(format!("Failed to create surreal client: {e:?}"))
-    })?;
-  (*surreal_client)
-    .use_ns("main")
-    .use_db("main")
-    .await
-    .map_err(|e| {
-      ServerFnError::new(format!("Failed to use namespace/db: {e:?}"))
-    })?;
-
-  // select the photo
-  let photo: Option<core_types::Photo> =
-    (*surreal_client).select(photo_id).await.map_err(|e| {
-      ServerFnError::new(format!("Failed to select photo: {e:?}"))
-    })?;
-  let photo = photo.ok_or_else(|| {
-    ServerFnError::new(format!("Photo not found: {photo_id:?}"))
-  })?;
-
-  // select the thumbnail artifact
-  let thumbnail_artifact: Option<core_types::PublicArtifact> =
-    (*surreal_client)
-      .select(photo.artifacts.thumbnail.artifact_id)
-      .await
-      .map_err(|e| {
-        ServerFnError::new(format!(
-          "Failed to select thumbnail artifact: {e:?}"
-        ))
-      })?;
-  let mut thumbnail_artifact = thumbnail_artifact.ok_or_else(|| {
-    ServerFnError::new("Thumbnail artifact not found".to_string())
-  })?;
-
-  // download the thumbnail artifact and get the content
-  thumbnail_artifact.download().await.map_err(|e| {
-    ServerFnError::new(format!("Failed to download thumbnail: {e:?}"))
-  })?;
-  let thumbnail_artifact_content = match thumbnail_artifact.contents {
-    Some(content) => content,
-    None => {
-      return Err(ServerFnError::new(
-        "Thumbnail artifact content not found".to_string(),
-      ))
-    }
-  };
-
-  let data = BASE64_STANDARD.encode(&thumbnail_artifact_content);
-
-  Ok(PhotoThumbnailDisplayParams {
-    data,
-    alt: "Thumbnail".to_string(),
-    size: photo.artifacts.thumbnail.size,
-  })
+      let error = format!("Failed to fetch photo thumbnail: {:?}", e);
+      tracing::error!("{error}");
+      ServerFnError::new(error)
+    })
 }
