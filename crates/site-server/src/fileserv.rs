5812a5b00f671349f7bb6fc63d14f1ed8facaefe
John Lewis <github@jlewis.sh>
2024-02-27 15:43:37 -0600

    feat: added static file caching in prod mode

--- crates/site-server/src/fileserv.rs
+++ crates/site-server/src/fileserv.rs
@@ -18,7 +18,13 @@ pub async fn file_and_error_handler(
   req: Request<Body>,
 ) -> AxumResponse {
   let root = app_state.leptos_options.site_root.clone();
-  let res = get_static_file(uri.clone(), &root).await.unwrap();
+  let res = get_static_file(
+    uri.clone(),
+    &root,
+    matches!(app_state.leptos_options.env, leptos_config::Env::PROD),
+  )
+  .await
+  .unwrap();
 
   if res.status() == StatusCode::OK {
     res.into_response()
@@ -39,6 +45,7 @@ pub async fn file_and_error_handler(
 async fn get_static_file(
   uri: Uri,
   root: &str,
+  cache: bool,
 ) -> Result<Response<Body>, (StatusCode, String)> {
   let req = Request::builder()
     .uri(uri.clone())
@@ -47,7 +54,16 @@ async fn get_static_file(
   // `ServeDir` implements `tower::Service` so we can call it with
   // `tower::ServiceExt::oneshot` This path is relative to the cargo root
   match ServeDir::new(root).oneshot(req).await {
-    Ok(res) => Ok(res.into_response()),
+    Ok(res) => {
+      let mut response = res.into_response();
+      if cache {
+        response.headers_mut().insert(
+          "Cache-Control",
+          "public, max-age=31536000, immutable".parse().unwrap(),
+        );
+      }
+      Ok(response)
+    }
     Err(err) => Err((
       StatusCode::INTERNAL_SERVER_ERROR,
       format!("Something went wrong: {err}"),
