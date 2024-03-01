a13a469233db31c590e6aa730aa1d1a23eb653f0
John Lewis <github@jlewis.sh>
2024-03-01 13:25:51 -0600

    feat: show photo deck on qr code page

--- crates/site-app/src/pages/qr_code.rs
+++ crates/site-app/src/pages/qr_code.rs
@@ -26,11 +26,22 @@ pub fn InnerQrCodePage(
     std::env::var("APP_BASE_URL").expect("APP_BASE_URL not set"),
   );
 
+  // we will display the qr code regardless, but if we can map it to a photo
+  // group, we will also display the photo deck
+  let photo_deck_element = match id.parse::<core_types::Ulid>() {
+    Ok(ulid) => view! {
+      <PhotoDeckWrapper group_id={core_types::PhotoGroupRecordId(ulid)} />
+    }
+    .into_view(),
+    Err(_) => view! { <p>"Invalid photo group ID"</p> }.into_view(),
+  };
+
   view! {
     <SmallPageWrapper extra_class=class theme_override=theme_override>
       <div class="d-card-body gap-4">
         <p class="text-2xl font-semibold tracking-tight">"QR Code"</p>
         <QrCode data=url class="rounded-box border shadow" />
+        {photo_deck_element}
         <div class="flex flex-row items-center gap-4">
           <a href="/dashboard" class="d-btn d-btn-primary d-btn-sm">"Back to Dashboard"</a>
           <div class="flex-1" />
@@ -60,6 +71,39 @@ pub fn QrCode(
             <div>
               <p>{e.to_string()}</p>
               <p>"Failed to generate QR code"</p>
+            </div>
+          }.into_view(),
+        }
+      })}
+    </Suspense>
+  }
+}
+
+#[component]
+pub fn PhotoDeckWrapper(
+  group_id: core_types::PhotoGroupRecordId,
+) -> impl IntoView {
+  let photo_group =
+    create_resource(move || group_id, bl::fetch::fetch_photo_group);
+
+  view! {
+    <Suspense fallback=|| view!{}>
+      { photo_group.map(|r| {
+        match r {
+          Ok(Some(photo_group)) => view! {
+            <div class="flex flex-row justify-center">
+              <crate::components::photo_deck::PhotoDeck ids={photo_group.photos.clone()} />
+            </div>
+          }.into_view(),
+          Ok(None) => view! {
+            <div>
+              <p>"Photo group not found"</p>
+            </div>
+          }.into_view(),
+          Err(e) => view! {
+            <div>
+              <p>{e.to_string()}</p>
+              <p>"Failed to fetch photo group"</p>
             </div>
           }.into_view(),
         }
