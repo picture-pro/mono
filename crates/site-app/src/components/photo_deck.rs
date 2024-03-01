ca62660321a834122889f4ee16b7706f4583e6e6
John Lewis <github@jlewis.sh>
2024-03-01 15:01:51 -0600

    feat: sophisticate `PhotoDeck`

--- crates/site-app/src/components/photo_deck.rs
+++ crates/site-app/src/components/photo_deck.rs
@@ -1,13 +1,48 @@
 use leptos::*;
 
+use crate::components::photo::Photo;
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Default)]
+pub enum PhotoDeckDisplayMode {
+  Flat,
+  #[default]
+  Stacked,
+}
+
 #[component]
-pub fn PhotoDeck(ids: Vec<core_types::PhotoRecordId>) -> impl IntoView {
-  view! {
-    { ids.into_iter().map(|photo_id| {
+pub fn PhotoDeck(
+  ids: Vec<core_types::PhotoRecordId>,
+  #[prop(optional)] display_mode: PhotoDeckDisplayMode,
+) -> impl IntoView {
+  logging::log!(
+    "Rendering PhotoDeck with {} photos in {:?}",
+    ids.len(),
+    display_mode
+  );
+  match display_mode {
+    PhotoDeckDisplayMode::Flat => view! {
+      <div class="flex flex-wrap gap-2 items-center">
+        { ids.into_iter().map(|id| view! { <Photo photo_id={id} /> }).collect::<Vec<_>>() }
+      </div>
+    }
+    .into_view(),
+    PhotoDeckDisplayMode::Stacked => {
+      let count = ids.len();
       view! {
-        <crate::components::photo::Photo photo_id=photo_id />
+        <div class="grid">
+          { ids.into_iter().enumerate().map(|(i, id)| view! {
+            <Photo
+              photo_id={id}
+              rotation={i as f32 * 16.0}
+              scale={1.0 - (i as f32 * 0.05)}
+              z_index={count as i32 - i as i32}
+              opacity={1.0 - (i as f32 * 0.1)}
+              extra_class="col-start-1 row-start-1"
+            />
+          }).collect::<Vec<_>>() }
+        </div>
       }
       .into_view()
-    }).collect::<Vec<_>>() }
+    }
   }
 }
