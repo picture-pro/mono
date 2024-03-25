1fdc8e1b3ba3825331aa2e24058b1dc12520ed0b
John Lewis <github@jlewis.sh>
2024-03-25 16:35:09 +0100

    org: split `HomePage` component

--- crates/site-app/src/pages/home_page.rs
+++ crates/site-app/src/pages/home_page.rs
@@ -4,28 +4,42 @@ use crate::components::basic::Link;
 
 #[component]
 pub fn HomePage() -> impl IntoView {
-  let user = crate::utils::authenticated_user();
+  match crate::utils::authenticated_user() {
+    Some(_) => AuthenticatedHomePage().into_view(),
+    None => UnauthenticatedHomePage().into_view(),
+  }
+}
+
+#[component]
+pub fn AuthenticatedHomePage() -> impl IntoView {
+  // we only call this when the user is authenticated, so unwrap is safe
+  let user = crate::utils::authenticated_user().unwrap();
 
   view! {
     <super::SmallPageWrapper>
       <div class="flex flex-col p-8 gap-4">
         <p class="text-2xl font-semibold tracking-tight">"Welcome to PicturePro!"</p>
-        { match user {
-          Some(user) => view! {
-            <p>{format!("You are logged in as {} ({})", user.name, user.email)}</p>
-          }.into_view(),
-          None => view! {
-            <p>
-              "You are not logged in."
-            </p>
-            <p>"Please "
-              <Link href="/login".to_string()>login</Link>
-              " or "
-              <Link href="/signup".to_string()>"sign up"</Link>
-              "."
-            </p>
-          }.into_view()
-        }}
+        <p>{format!("You are logged in as {} ({})", user.name, user.email)}</p>
+      </div>
+    </super::SmallPageWrapper>
+  }
+}
+
+#[component]
+pub fn UnauthenticatedHomePage() -> impl IntoView {
+  view! {
+    <super::SmallPageWrapper>
+      <div class="flex flex-col p-8 gap-4">
+        <p class="text-2xl font-semibold tracking-tight">"Welcome to PicturePro!"</p>
+        <p>
+          "You are not logged in."
+        </p>
+        <p>"Please "
+          <Link href="/login".to_string()>login</Link>
+          " or "
+          <Link href="/signup".to_string()>"sign up"</Link>
+          "."
+        </p>
       </div>
     </super::SmallPageWrapper>
   }
