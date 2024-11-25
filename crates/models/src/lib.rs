e0bd8562f7629e3cee4d264f22fe535389ba2981
John Lewis <github@jlewis.sh>
2024-11-25 12:55:18 +0100

    feat: add `Photo` model

--- crates/models/src/lib.rs
+++ crates/models/src/lib.rs
@@ -1,5 +1,6 @@
 //! Domain models for the PicturePro project.
 
 mod artifact;
+mod photo;
 
-pub use self::artifact::*;
+pub use self::{artifact::*, photo::*};
---
+++ crates/models/src/photo.rs
@@ -0,0 +1,38 @@
+use model::{Model, RecordId};
+use serde::{Deserialize, Serialize};
+
+use crate::ArtifactRecordId;
+
+/// The table name for [`Photo`] records.
+pub const PHOTO_TABLE_NAME: &str = "photo";
+
+/// An alias for [`RecordId<Photo>`].
+pub type PhotoRecordId = RecordId<Photo>;
+
+/// The domain model for a photo stored on the platform.
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Photo {
+  /// The photo's ID.
+  pub id:        PhotoRecordId,
+  /// The photo's artifacts.
+  pub artifacts: PhotoArtifacts,
+}
+
+/// The artifacts for a [`Photo`].
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct PhotoArtifacts {
+  /// The photo's original artifact.
+  pub original:  ArtifactRecordId,
+  /// The photo's thumbnail artifact.
+  pub thumbnail: ArtifactRecordId,
+}
+
+impl Model for Photo {
+  const TABLE_NAME: &'static str = PHOTO_TABLE_NAME;
+  const UNIQUE_INDICES: &'static [(
+    &'static str,
+    model::SlugFieldGetter<Self>,
+  )] = &[];
+
+  fn id(&self) -> PhotoRecordId { self.id }
+}
