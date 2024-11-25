12792a244208a1182e0a402cea479cdd872ca7ad
John Lewis <github@jlewis.sh>
2024-11-25 13:21:48 +0100

    feat(prime-domain): drafted basic `PrimeDomainService` trait

--- crates/prime-domain/src/lib.rs
+++ crates/prime-domain/src/lib.rs
@@ -1,2 +1,29 @@
 //! Provides prime-domain Services, the entry points for domain-specific
 //! business logic.
+
+mod canonical;
+
+use std::sync::Arc;
+
+pub use hex;
+use hex::Hexagonal;
+use models::{Artifact, ArtifactRecordId, Photo, PhotoRecordId};
+use repos::db::FetchModelError;
+
+/// A dynamic [`PrimeDomainService`] trait object.
+pub type DynPrimeDomainService = Arc<Box<dyn PrimeDomainService>>;
+
+/// The prime domain service trait.
+#[async_trait::async_trait]
+pub trait PrimeDomainService: Hexagonal {
+  /// Fetch an [`Artifact`] by its ID.
+  async fn fetch_cache_by_id(
+    &self,
+    id: ArtifactRecordId,
+  ) -> Result<Option<Artifact>, FetchModelError>;
+  /// Fetch a [`Photo`] by its ID.
+  async fn fetch_photo_by_id(
+    &self,
+    id: PhotoRecordId,
+  ) -> Result<Option<Photo>, FetchModelError>;
+}
