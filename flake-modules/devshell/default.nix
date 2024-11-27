e9b7df1b3c5b3f73c2507a268ef529e48dbbe109
John Lewis <github@jlewis.sh>
2024-11-27 11:38:42 +0100

    feat(devshell): add `serve` command

--- flake-modules/devshell/default.nix
+++ flake-modules/devshell/default.nix
@@ -91,6 +91,12 @@ localFlake: { ... }: {
           help = "Runs ${bin-hl "cargo-leptos"} in watch mode.";
           category = "[build actions]";
         }
+        {
+          name = "serve";
+          command = "cargo leptos serve --release $@";
+          help = "Runs ${bin-hl "cargo-leptos"} in watch mode.";
+          category = "[build actions]";
+        }
       ]
         ++ tikv-docker-commands
         ;
