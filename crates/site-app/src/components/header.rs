4bf93c2630dc13bdc6d41379ed80aa73fd183c7a
John Lewis <github@jlewis.sh>
2024-12-11 14:11:58 +0100

    feat(site): more intelligent header

--- crates/site-app/src/components/header.rs
+++ crates/site-app/src/components/header.rs
@@ -1,5 +1,6 @@
-use leptos::prelude::*;
+use leptos::{either::Either, prelude::*};
 use lsc::{button::*, link::*};
+use models::PublicUser;
 
 use crate::AuthStatus;
 
@@ -8,9 +9,6 @@ pub fn Header() -> impl IntoView {
   let class = "sticky top-0 container bg-base-subtle h-12 mx-auto px-3 flex \
                flex-row items-center rounded-b-xl border border-t-0 \
                border-base-7 dark:border-basedark-7";
-
-  let auth_status = use_context::<AuthStatus>().and_then(|as_| as_.0);
-  let auth_status_text = format!("Status: {:?}", auth_status);
 
   view! {
     <header class=class>
@@ -21,14 +19,45 @@ pub fn Header() -> impl IntoView {
       </span>
       <div class="flex-1" />
       <div class="flex flex-row items-center gap-2">
-        <p>{ auth_status_text }</p>
-        <Button href="/sign-up" color=ButtonColor::Primary>
-          "Sign Up"
-        </Button>
-        <Button href="/log-in" color=ButtonColor::Base>
-          "Log In"
-        </Button>
+        <HeaderUserArea />
       </div>
     </header>
+  }
+}
+
+#[component]
+fn HeaderUserArea() -> impl IntoView {
+  let auth_status = use_context::<AuthStatus>().and_then(|as_| as_.0);
+
+  match auth_status {
+    Some(user) => Either::Left(view! { <LoggedInUserAuthActions user=user /> }),
+    None => Either::Right(view! { <LoggedOutUserAuthActions /> }),
+  }
+}
+
+#[component]
+fn LoggedOutUserAuthActions() -> impl IntoView {
+  view! {
+    <Button href="/sign-up" color=ButtonColor::Primary>
+      "Sign Up"
+    </Button>
+    <Button href="/log-in" color=ButtonColor::Base>
+      "Log In"
+    </Button>
+  }
+}
+
+#[component]
+fn LoggedInUserAuthActions(user: PublicUser) -> impl IntoView {
+  view! {
+    <span class="text-sm text-basea-11 dark:text-basedarka-11">
+      "Welcome, "
+      <span class="text-primarya-11 dark:text-primarydarka-11">
+        { user.name.to_string() }
+      </span>
+    </span>
+    <Button href="/log-out" color=ButtonColor::Base>
+      "Log Out"
+    </Button>
   }
 }
