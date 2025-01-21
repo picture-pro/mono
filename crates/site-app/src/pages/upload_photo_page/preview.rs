94d5dd2a380469e4a62b9d6f4ca52160c4f848b0
John Lewis <github@jlewis.sh>
2025-01-21 21:58:13 +0100

    feat(site): upload: more stylistic changes

--- crates/site-app/src/pages/upload_photo_page/preview.rs
+++ crates/site-app/src/pages/upload_photo_page/preview.rs
@@ -9,8 +9,8 @@ pub(super) fn ImagePreviewer() -> impl IntoView {
 
   let indices_iter = move || context.iter_file_indices();
 
-  let grid_class = "grid sm:grid-cols-[repeat(auto-fit,12rem)] \
-                    grid-cols-[repeat(auto-fit,8rem)] gap-4";
+  let grid_class =
+    "grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 grid-cols-3 gap-4";
 
   view! {
     <div class=grid_class>
@@ -39,14 +39,18 @@ fn ImagePreview(index: usize) -> impl IntoView {
       .is_some_and(|v| v)
   };
 
-  let image_class = "w-auto sm:max-h-[12rem] max-h-[8rem] border-2 \
-                     border-base-8 dark:border-basedark-8 rounded-lg";
+  let image_class = "w-full sm:max-h-48 max-h-40 border-2 border-base-8 \
+                     dark:border-basedark-8 group-hover:border-primary-8 \
+                     group-hover:dark:border-primarydark-8 ring-2 \
+                     ring-transparent group-hover:ring-primary-8 \
+                     group-hover:dark:ring-primarydark-8 transition-colors \
+                     rounded-lg";
 
   let oversized_overlay_class =
-    "absolute inset-0 flex flex-col gap-2 items-center justify-center \
-     bg-base-1/[.8] dark:bg-basedark-1/[.8] rounded-lg border-2 \
-     border-warning-8 dark:border-warningdark-8 text-center text-warning-12 \
-     dark:text-warningdark-12 backdrop-blur-sm";
+    "absolute inset-0 flex flex-col sm:gap-2 gap-1 p-2 items-center \
+     justify-center bg-base-1/[.8] dark:bg-basedark-1/[.8] rounded-lg \
+     border-2 border-warning-8 dark:border-warningdark-8 text-center \
+     text-warning-12 dark:text-warningdark-12 backdrop-blur-sm";
   let file_size = move || {
     context
       .get_file(index)
@@ -55,11 +59,11 @@ fn ImagePreview(index: usize) -> impl IntoView {
   let oversized_overlay_element = move || {
     is_oversized().then_some(view! {
       <div class=oversized_overlay_class>
-        <ExclamationTriangleIcon {..} class="size-10" />
+        <ExclamationTriangleIcon {..} class="sm:size-10 size-8" />
         <div>
-          <p class="text-xl font-bold">"Oversized"</p>
-          <p class="text-sm text-warning-dim">
-            "This image is too large to upload. File: "
+          <p class="sm:text-lg font-bold">"Oversized"</p>
+          <p class="sm:text-sm text-xs text-warning-dim">
+            "Image is too large to upload. File: "
             { move || file_size().map(|fs| fs.to_string()) }
             ", max: "
             { FileSize::new(MAX_UPLOAD_SIZE).to_string() }
@@ -72,29 +76,31 @@ fn ImagePreview(index: usize) -> impl IntoView {
   let delete_button_class =
     "absolute top-0 right-0 size-8 flex flex-col justify-center items-center \
      bg-base-2 dark:bg-basedark-2 border-2 border-base-8 \
-     dark:border-basedark-8 rounded-bl-lg rounded-tr-lg cursor-pointer";
+     dark:border-basedark-8 hover:border-danger-8 \
+     hover:dark:border-dangerdark-8 rounded-bl-lg rounded-tr-lg \
+     cursor-pointer text-base-dim hover:text-danger-dim transition-colors";
   let delete_handler = move |_| {
     context.delete_file(index);
   };
   let delete_button_element = move || {
     view! {
       <div class=delete_button_class on:click=delete_handler>
-        <TrashIcon {..} class="size-6 text-base-dim" />
+        <TrashIcon {..} class="size-6" />
       </div>
     }
   };
 
   move || {
     url().map(|url| {
-  view! {
-    <div class="sm:size-48 size-32 flex flex-col justify-center items-center">
-      <div class="relative">
-        <img src={url} class=image_class />
-        { oversized_overlay_element }
-        { delete_button_element }
-      </div>
-    </div>
-  }
-})
+      view! {
+        <div class="flex flex-col justify-center items-center group">
+          <div class="relative">
+            <img src={url} class=image_class />
+            { oversized_overlay_element }
+            { delete_button_element }
+          </div>
+        </div>
+      }
+    })
   }
 }
