0445da59dce17df97aea6bc54a46ea1fe84c9420
John Lewis <github@jlewis.sh>
2025-03-17 12:10:30 +0100

    chore(app): clean house for upload flow

--- crates/site-app/src/pages/mod.rs
+++ crates/site-app/src/pages/mod.rs
@@ -4,9 +4,8 @@ mod logout_page;
 mod not_found_page;
 mod profile_page;
 mod signup_page;
-mod upload_photo_page;
 
 pub use self::{
   home_page::*, login_page::*, logout_page::*, not_found_page::*,
-  profile_page::*, signup_page::*, upload_photo_page::*,
+  profile_page::*, signup_page::*,
 };
