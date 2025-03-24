5dfd4ba8ccfbf89fd1caee55c735e1437fb33d13
John Lewis <github@jlewis.sh>
2025-03-24 13:59:19 +0100

    feat(app): automatic upload process drafted

--- crates/site-app/src/pages/upload_photo_page/photo.rs
+++ crates/site-app/src/pages/upload_photo_page/photo.rs
@@ -1,7 +1,8 @@
-use std::fmt;
+use std::{fmt, sync::Arc};
 
 use gloo::file::{Blob, File, ObjectUrl};
-use models::Ulid;
+use leptos::prelude::{Action, LocalStorage};
+use models::{ArtifactRecordId, Ulid};
 use reactive_stores::Store;
 use send_wrapper::SendWrapper;
 
@@ -21,18 +22,19 @@ impl Photo {
 
     let blob = Blob::from(file);
     let blob_size = blob.size();
+    let url = ObjectUrl::from(blob.clone());
+    let blob = SendWrapper::new(blob);
     let upload_state = match blob_size > MAX_UPLOAD_SIZE {
       true => {
         leptos::logging::warn!("photo {id} is too large ({blob_size})");
         PhotoUploadState::Oversized
       }
-      false => PhotoUploadState::UploadQueued,
+      false => PhotoUploadState::UploadQueued(blob.clone()),
     };
-    let url = ObjectUrl::from(blob.clone());
 
     Self {
       id,
-      blob: SendWrapper::new(blob),
+      blob,
       url: SendWrapper::new(url),
       upload_state,
     }
@@ -57,10 +59,66 @@ impl fmt::Debug for Photo {
   }
 }
 
-#[derive(Store, Debug)]
 pub(super) enum PhotoUploadState {
-  UploadQueued,
-  UploadInProgress,
-  UploadFinished,
+  UploadQueued(SendWrapper<Blob>),
+  UploadInProgress(
+    Action<SendWrapper<Blob>, Result<ArtifactRecordId, String>, LocalStorage>,
+  ),
+  UploadFinished(Result<ArtifactRecordId, String>),
   Oversized,
+}
+
+impl fmt::Debug for PhotoUploadState {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      Self::UploadQueued(_) => write!(f, "UploadQueued"),
+      Self::UploadInProgress(_) => f.debug_tuple("UploadInProgress").finish(),
+      Self::UploadFinished(arg0) => {
+        f.debug_tuple("UploadFinished").field(arg0).finish()
+      }
+      Self::Oversized => write!(f, "Oversized"),
+    }
+  }
+}
+
+impl PhotoUploadState {
+  pub(super) fn advance(&mut self) {
+    match &*self {
+      PhotoUploadState::UploadQueued(blob) => {
+        let action = Action::new_local(move |blob| {
+          upload_action_fn(SendWrapper::clone(blob))
+        });
+        action.dispatch_local(blob.clone());
+        *self = PhotoUploadState::UploadInProgress(action);
+      }
+      PhotoUploadState::UploadInProgress(action) => {
+        if let Some(value) = action.value()() {
+          *self = PhotoUploadState::UploadFinished(value);
+        }
+      }
+      PhotoUploadState::UploadFinished(_) => {}
+      PhotoUploadState::Oversized => {}
+    }
+  }
+}
+
+async fn upload_action_fn(
+  blob: SendWrapper<Blob>,
+) -> Result<ArtifactRecordId, String> {
+  use gloo::net::http::*;
+
+  let request = Request::post("/api/upload_artifact")
+    .body(blob.take())
+    .expect("failed to set blob as body of upload_artifact request");
+
+  let response = request
+    .send()
+    .await
+    .map_err(|e| format!("failed to send upload_artifact request: {e}"))?;
+
+  let value: ArtifactRecordId = response.json().await.map_err(|e| {
+    format!("failed to deserialize upload_artifact response: {e}")
+  })?;
+
+  Ok(value)
 }
