7ebe5eadab82c90c7584fe2fad58bc6338156f75
John Lewis <github@jlewis.sh>
2025-03-27 18:04:11 +0100

    feat(repos): concretize `ArtifactRepository`

--- crates/repos/src/artifact.rs
+++ crates/repos/src/artifact.rs
@@ -1,13 +1,17 @@
-mod canonical;
+use std::sync::Arc;
 
 use db::{CreateModelError, FetchModelByIndexError, FetchModelError};
-use hex::Hexagonal;
-use models::{Artifact, ArtifactPath, ArtifactRecordId, UserRecordId};
+use hex::health;
+use models::{
+  Artifact, ArtifactCreateRequest, ArtifactPath, ArtifactRecordId,
+  CompressionStatus, FileSize, StrictSlug, UserRecordId,
+};
 use storage::{
-  belt::Belt, ReadError as StorageReadError, WriteError as StorageWriteError,
+  belt::Belt, ReadError as StorageReadError, StorageClient,
+  WriteError as StorageWriteError,
 };
 
-pub use self::canonical::*;
+use crate::ModelRepositoryLike;
 
 /// An error that occurs when reading the data of an [`Artifact`].
 #[derive(Debug, thiserror::Error, miette::Diagnostic)]
@@ -34,60 +38,71 @@ pub enum CreateArtifactError {
   StorageWriteError(StorageWriteError),
 }
 
-/// Defines a repository interface for [`Artifact`] models.
-#[async_trait::async_trait]
-pub trait ArtifactRepository: Hexagonal {
-  /// Fetches an [`Artifact`] by its ID.
-  async fn fetch_artifact_by_id(
-    &self,
-    id: ArtifactRecordId,
-  ) -> Result<Option<Artifact>, FetchModelError>;
-
-  /// Fetches an [`Artifact`] by its path.
-  async fn fetch_artifact_by_path(
-    &self,
-    path: ArtifactPath,
-  ) -> Result<Option<Artifact>, FetchModelByIndexError>;
-
-  /// Reads the data of an [`Artifact`] by its ID.
-  async fn read_artifact_by_id(
-    &self,
-    id: ArtifactRecordId,
-  ) -> Result<Option<Belt>, ReadArtifactError>;
-
-  /// Creates a new [`Artifact`].
-  async fn create_artifact(
-    &self,
-    data: Belt,
-    originator: UserRecordId,
-  ) -> Result<Artifact, CreateArtifactError>;
+pub struct ArtifactRepository {
+  storage_repo: StorageClient,
+  model_repo: Arc<
+    dyn ModelRepositoryLike<
+      Model = Artifact,
+      ModelCreateRequest = ArtifactCreateRequest,
+      CreateError = CreateArtifactError,
+    >,
+  >,
 }
 
 #[async_trait::async_trait]
-impl<T, I> ArtifactRepository for T
-where
-  T: std::ops::Deref<Target = I> + Hexagonal + Sized,
-  I: ArtifactRepository + ?Sized,
-{
+impl health::HealthReporter for ArtifactRepository {
+  fn name(&self) -> &'static str { stringify!(TempStorageRepositoryCanonical) }
+  async fn health_check(&self) -> health::ComponentHealth {
+    health::AdditiveComponentHealth::from_futures(vec![
+      self.model_repo.health_report(),
+      self.storage_repo.health_report(),
+    ])
+    .await
+    .into()
+  }
+}
+
+impl ArtifactRepository {
   async fn fetch_artifact_by_id(
     &self,
     id: ArtifactRecordId,
   ) -> Result<Option<Artifact>, FetchModelError> {
-    I::fetch_artifact_by_id(self, id).await
+    self.model_repo.fetch_model_by_id(id).await
   }
 
   async fn fetch_artifact_by_path(
     &self,
     path: ArtifactPath,
   ) -> Result<Option<Artifact>, FetchModelByIndexError> {
-    I::fetch_artifact_by_path(self, path).await
+    self
+      .model_repo
+      .fetch_model_by_index(
+        "path".into(),
+        models::EitherSlug::Strict(StrictSlug::new(path.to_string())),
+      )
+      .await
   }
 
   async fn read_artifact_by_id(
     &self,
     id: ArtifactRecordId,
   ) -> Result<Option<Belt>, ReadArtifactError> {
-    I::read_artifact_by_id(self, id).await
+    let artifact = self
+      .model_repo
+      .fetch_model_by_id(id)
+      .await
+      .map_err(ReadArtifactError::FetchModelError)?;
+    match artifact {
+      Some(artifact) => {
+        let data = self
+          .storage_repo
+          .read(&artifact.path.to_path_buf())
+          .await
+          .map_err(ReadArtifactError::StorageReadError)?;
+        Ok(Some(data))
+      }
+      None => Ok(None),
+    }
   }
 
   async fn create_artifact(
@@ -95,6 +110,30 @@ where
     data: Belt,
     originator: UserRecordId,
   ) -> Result<Artifact, CreateArtifactError> {
-    I::create_artifact(self, data, originator).await
+    let pre_comp_counter = data.counter();
+    let data = data.adapt_to_comp(storage::belt::CompressionAlgorithm::Zstd);
+
+    let path = ArtifactPath::new_random();
+    let post_comp_size = self
+      .storage_repo
+      .write(&path.to_path_buf(), data)
+      .await
+      .map_err(CreateArtifactError::StorageWriteError)?;
+
+    let comp_status = CompressionStatus::Compressed {
+      compressed_size:   post_comp_size,
+      uncompressed_size: FileSize::new(pre_comp_counter.current()),
+      algorithm:         models::CompressionAlgorithm::Zstd,
+    };
+
+    let artifact = self
+      .model_repo
+      .create_model(ArtifactCreateRequest {
+        path,
+        originator,
+        comp_status,
+      })
+      .await?;
+    Ok(artifact)
   }
 }
