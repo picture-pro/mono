03eb95b8bbf1ebb2c68c1595a35a506574cd2283
John Lewis <github@jlewis.sh>
2025-03-31 11:40:35 +0200

    feat(prime-domain): wipe and draft concretized service

--- crates/prime-domain/src/lib.rs
+++ crates/prime-domain/src/lib.rs
@@ -11,54 +11,37 @@ use miette::Result;
 pub use models;
 use models::{Artifact, Photo, PhotoRecordId, UserRecordId};
 pub use repos;
-use repos::{belt::Belt, CreateArtifactError, FetchModelError};
+use repos::{
+  belt::Belt, ArtifactRepository, CreateArtifactError, FetchModelError,
+  PhotoRepository,
+};
 
-pub use self::canonical::*;
-
-/// A dynamic [`PrimeDomainService`] trait object.
-pub type DynPrimeDomainService = Arc<Box<dyn PrimeDomainService>>;
-
-/// The prime domain service trait.
-#[async_trait::async_trait]
-pub trait PrimeDomainService: Hexagonal {
-  /// Fetch a [`Photo`] by its ID.
-  async fn fetch_photo_by_id(
-    &self,
-    id: PhotoRecordId,
-  ) -> Result<Option<Photo>, FetchModelError>;
-  /// Produce a list of all [`Photo`]s.
-  async fn enumerate_photos(&self) -> Result<Vec<Photo>>;
-
-  /// Create an artifact.
-  async fn create_artifact(
-    &self,
-    data: Belt,
-    originator: UserRecordId,
-  ) -> Result<Artifact, CreateArtifactError>;
+#[derive(Debug, Clone)]
+pub struct PrimeDomainService {
+  photo_repo:    PhotoRepository,
+  artifact_repo: ArtifactRepository,
 }
 
 #[async_trait::async_trait]
-impl<T, I> PrimeDomainService for T
-where
-  T: std::ops::Deref<Target = I> + Hexagonal + Sized,
-  I: PrimeDomainService + ?Sized,
-{
-  async fn fetch_photo_by_id(
-    &self,
-    id: PhotoRecordId,
-  ) -> Result<Option<Photo>, FetchModelError> {
-    self.deref().fetch_photo_by_id(id).await
+impl health::HealthReporter for PrimeDomainService {
+  fn name(&self) -> &'static str { stringify!(PrimeDomainService) }
+  async fn health_check(&self) -> health::ComponentHealth {
+    health::AdditiveComponentHealth::from_futures(vec![
+      self.photo_repo.health_report(),
+      self.artifact_repo.health_report(),
+    ])
+    .await
+    .into()
   }
+}
 
-  async fn enumerate_photos(&self) -> Result<Vec<Photo>> {
-    self.deref().enumerate_photos().await
-  }
-
-  async fn create_artifact(
+impl PrimeDomainService {
+  #[instrument(skip(self))]
+  pub async fn create_artifact(
     &self,
     data: Belt,
     originator: UserRecordId,
-  ) -> Result<Artifact, CreateArtifactError> {
-    self.deref().create_artifact(data, originator).await
+  ) -> Result<Artiact, CreateArtifactError> {
+    self.artifact_repo.create_artifact(data, originator).await
   }
 }
