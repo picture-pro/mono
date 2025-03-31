d873c56e083ac4c819e351989633605e984fbbef
John Lewis <github@jlewis.sh>
2025-03-31 11:40:35 +0200

    fix(repos): export repositories

--- crates/repos/src/lib.rs
+++ crates/repos/src/lib.rs
@@ -11,6 +11,8 @@ use miette::Result;
 use models::EitherSlug;
 pub use storage::{self, belt};
 
+pub use self::{artifact::*, photo::*};
+
 /// Defines a repository interface for models.
 #[async_trait::async_trait]
 pub trait ModelRepositoryLike: Hexagonal {
