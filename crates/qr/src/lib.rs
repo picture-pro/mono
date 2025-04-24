00a9cbcc91f437689e50f55d85053ac2f2bd62b0
John Lewis <github@jlewis.sh>
2025-04-24 17:09:15 +0200

    feat(qr): init qr crate

---
+++ crates/qr/src/lib.rs
@@ -0,0 +1,28 @@
+//! Provides domain-specific methods for creating QR codes.
+
+use fast_qr::{QRBuilder, convert::svg::SvgBuilder, qr::QRCodeError};
+use models::PhotoGroupRecordId;
+
+/// Provides domain-specific methods for creating QR codes.
+#[derive(Clone, Debug)]
+pub struct QrCodeGenerator(());
+
+impl QrCodeGenerator {
+  /// Create a new [`QrCodeGenerator`].
+  #[expect(clippy::new_without_default)]
+  pub fn new() -> Self { QrCodeGenerator(()) }
+
+  /// Generates a QR code for a given [`PhotoGroup`].
+  pub fn generate_photo_group_link(
+    base_url: &str,
+    id: PhotoGroupRecordId,
+  ) -> Result<String, QRCodeError> {
+    const PHOTO_GROUP_PATH: &str = "/photo-group/";
+
+    let full_url = format!("{base_url}{PHOTO_GROUP_PATH}{id}");
+
+    let qr = QRBuilder::new(full_url).build()?;
+
+    Ok(SvgBuilder::default().to_str(&qr))
+  }
+}
