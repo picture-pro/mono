4a1fbcc7319d615b0ef4b121cbd0775093413620
John Lewis <github@jlewis.sh>
2025-04-24 15:49:13 +0200

    feat(app): load data fully on photo group page

--- crates/site-app/src/pages/photo_group_page.rs
+++ crates/site-app/src/pages/photo_group_page.rs
@@ -1,11 +1,11 @@
 use std::str::FromStr;
 
-use base_components::{Section, Title};
+use base_components::{Section, SmallImage, Title};
 use leptos::{either::Either, prelude::*};
 use leptos_router::hooks::use_params_map;
-use models::PhotoGroupRecordId;
+use models::{PhotoGroup, PhotoGroupRecordId};
 
-use crate::pages::NotFoundPage;
+use crate::{pages::NotFoundPage, server_fns::fetch_photo_group};
 
 fn extract_photo_group_id() -> Signal<Option<PhotoGroupRecordId>> {
   let params = use_params_map();
@@ -22,17 +22,62 @@ pub fn PhotoGroupPage() -> impl IntoView {
   let id = extract_photo_group_id();
 
   move || match id() {
-    Some(id) => Either::Left(view! { <PhotoGroupPageInner id=id /> }),
+    Some(id) => Either::Left(view! { <PhotoGroupFetcher id=id /> }),
     None => Either::Right(view! { <NotFoundPage /> }),
   }
 }
 
 #[component]
-pub fn PhotoGroupPageInner(id: PhotoGroupRecordId) -> impl IntoView {
+pub fn PhotoGroupFetcher(id: PhotoGroupRecordId) -> impl IntoView {
+  let pg_resource = Resource::new(move || id, fetch_photo_group);
+
+  let suspended_fn = move || {
+    Suspend::new(async move {
+      match pg_resource.await {
+        Ok(Some(pg)) => view! { <PhotoGroupPageInner pg=pg />}.into_any(),
+        Ok(None) => view! { <NotFoundPage /> }.into_any(),
+        Err(e) => {
+          let e = e.to_string();
+          view! { "failed to fetch photo groups: " {e} }.into_any()
+        }
+      }
+    })
+  };
+
+  view! {
+    <Suspense fallback=move || view! { "Loading..." }>
+      { suspended_fn }
+    </Suspense>
+  }
+}
+
+#[component]
+pub fn PhotoGroupPageInner(pg: PhotoGroup) -> impl IntoView {
   view! {
     <Section>
       <Title>"Photo Group"</Title>
-      <pre><code>"id = \"" { id.to_string() } "\""</code></pre>
+      <pre><code>"id = \"" { pg.id.to_string() } "\""</code></pre>
     </Section>
+    <Section>
+      <PhotoGroupDetails pg=pg />
+    </Section>
+  }
+}
+
+#[component]
+pub fn PhotoGroupDetails(pg: PhotoGroup) -> impl IntoView {
+  view! {
+    <div>
+      <p class="text-xl">{ pg.config.usage_rights_price.to_string() }</p>
+      <div class="flex flex-row flex-wrap gap-4">
+      <For
+        each=move || pg.photos.clone()
+        key=move |p| *p
+        children=move |p| view! {
+          <SmallImage url=format!("/api/photo_thumbnail/{p}") />
+        }
+      />
+      </div>
+    </div>
   }
 }
