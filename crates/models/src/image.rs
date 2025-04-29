8a0af2cc67630d2676a2fd07a087e0bf941cd407
John Lewis <github@jlewis.sh>
2025-04-29 17:41:18 +0200

    feat(models): add `Image` model

---
+++ crates/models/src/image.rs
@@ -0,0 +1,73 @@
+use model::{Model, RecordId};
+use serde::{Deserialize, Serialize};
+
+use crate::{ArtifactRecordId, EitherSlug, LaxSlug};
+
+/// The table name for [`Image`] records.
+pub const IMAGE_TABLE_NAME: &str = "image";
+
+/// An alias for [`RecordId<Image>`].
+pub type ImageRecordId = RecordId<Image>;
+
+/// An artifact-backed Image.
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Image {
+  /// The image's ID.
+  pub id:           ImageRecordId,
+  /// The [`Artifact`](crate::Artifact) backing the image.
+  pub artifact:     ArtifactRecordId,
+  /// The width of the image.
+  pub width:        u32,
+  /// The height of the image.
+  pub height:       u32,
+  /// A tiny preview of the image.
+  pub tiny_preview: ImageTinyPreview,
+}
+
+/// A tiny preview of an [`Image`].
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct ImageTinyPreview {
+  /// The width of the preview.
+  pub width:  u32,
+  /// The height of the preview.
+  pub height: u32,
+  /// The data of the preview.
+  pub data:   Vec<u8>,
+}
+
+impl Model for Image {
+  const TABLE_NAME: &'static str = IMAGE_TABLE_NAME;
+  const UNIQUE_INDICES: &'static [(
+    &'static str,
+    model::SlugFieldGetter<Self>,
+  )] = &[("artifact", |i| {
+    EitherSlug::Lax(LaxSlug::new(i.artifact.to_string()))
+  })];
+  const INDICES: &'static [(&'static str, model::SlugFieldGetter<Self>)] = &[];
+  fn id(&self) -> ImageRecordId { self.id }
+}
+
+/// A request to create a new [`Image`].
+#[derive(Debug)]
+pub struct ImageCreateRequest {
+  /// The [`Artifact`](crate::Artifact) backing the image.
+  pub artifact:     ArtifactRecordId,
+  /// The width of the image.
+  pub width:        u32,
+  /// The height of the image.
+  pub height:       u32,
+  /// A tiny preview of the image.
+  pub tiny_preview: ImageTinyPreview,
+}
+
+impl From<ImageCreateRequest> for Image {
+  fn from(value: ImageCreateRequest) -> Self {
+    Self {
+      id:           ImageRecordId::default(),
+      artifact:     value.artifact,
+      width:        value.width,
+      height:       value.height,
+      tiny_preview: value.tiny_preview,
+    }
+  }
+}
--- crates/models/src/lib.rs
+++ crates/models/src/lib.rs
@@ -1,6 +1,7 @@
 //! Domain models for the PicturePro project.
 
 mod artifact;
+mod image;
 mod photo;
 mod photo_group;
 mod user;
@@ -12,5 +13,5 @@ pub use dvf::{slugger::*, *};
 pub use model::*;
 
 pub use self::{
-  artifact::*, photo::*, photo_group::*, price::*, state::*, user::*,
+  artifact::*, image::*, photo::*, photo_group::*, price::*, state::*, user::*,
 };
