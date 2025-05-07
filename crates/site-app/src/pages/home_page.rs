4f320d5e0699754ba87c171476a954d7eb81809c
John Lewis <github@jlewis.sh>
2025-05-07 11:42:26 +0200

    feat(app): rebuild front page

--- crates/site-app/src/pages/home_page.rs
+++ crates/site-app/src/pages/home_page.rs
@@ -1,30 +1,70 @@
-use base_components::{Prose, Section, Title};
+use base_components::{Section, Title};
 use leptos::prelude::*;
-use lsc::{button::*, icons::*};
 
 #[component]
 pub fn HomePage() -> impl IntoView {
+  use lsc::icons::*;
+
   view! {
     <Section>
-      <div class="inline-flex flex-col items-start">
-        <Title>
-          "Welcome to "
+      <Title class="ml-4">
+        <span class="block">"Welcome to "</span>
+        <span class="block">
           <span class="font-semibold">"PicturePro"</span>
           "."
-        </Title>
-        <Prose>
-          "Sharing candids is now easier than ever. Cute moment at the park? Done. Posing at the Grand Canyon? Easy. "
-          <span class="font-semibold">"PicturePro"</span>
-          " is the best way to share your photos with friends and family."
-        </Prose>
-        <div class="h-8" />
-        <Button
-          element_type=ButtonElementType::Link color=ButtonColor::Primary
-          size={ButtonSize::Large} {..} href="/sign-up" class=("mx-8", move || true)
-        >
-          "Get started"
-          <ArrowRightIcon {..} class="size-5" />
-        </Button>
+        </span>
+      </Title>
+    </Section>
+
+    <Section>
+      <div class="grid sm:grid-cols-2 w-full gap-4">
+        <a href="/upload-photo" class="bg-ruby-3 dark:bg-rubydark-3 border border-ruby-normal text-rubya-normal flex flex-col gap-2 p-4 rounded-xl shadow-xl transition">
+          <div class="flex flex-row items-center gap-2">
+            <CameraIcon {..} class="size-5" />
+            <p class="text-lg">
+              "Single Subject"
+            </p>
+          </div>
+          <p class="text-rubya-dim max-w-prose text-sm">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor magna in lacus egestas tincidunt. Maecenas erat tellus, dapibus feugiat purus eget, tincidunt semper risus."
+          </p>
+        </a>
+
+        <div class="bg-jade-3 dark:bg-jadedark-3 border border-jade-normal text-jadea-normal flex flex-col gap-2 p-4 rounded-xl shadow-xl transition">
+          <div class="flex flex-row items-center gap-2">
+            <UploadIcon {..} class="size-5" />
+            <p class="text-lg">
+              "Upload to Marketplace"
+            </p>
+          </div>
+          <p class="text-jadea-dim max-w-prose text-sm">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor magna in lacus egestas tincidunt. Maecenas erat tellus, dapibus feugiat purus eget, tincidunt semper risus."
+          </p>
+        </div>
+
+        <div class="bg-cyan-3 dark:bg-cyandark-3 border border-cyan-normal text-cyana-normal flex flex-col gap-2 p-4 rounded-xl shadow-xl transition">
+          <div class="flex flex-row items-center gap-2">
+            <MagnifyingGlassIcon {..} class="size-5" />
+            <p class="text-lg">
+              "Explore Marketplace"
+            </p>
+          </div>
+          <p class="text-cyana-dim max-w-prose text-sm">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor magna in lacus egestas tincidunt. Maecenas erat tellus, dapibus feugiat purus eget, tincidunt semper risus."
+          </p>
+        </div>
+
+        <div class="bg-orange-3 dark:bg-orangedark-3 border border-orange-normal text-orangea-normal flex flex-col gap-2 p-4 rounded-xl shadow-xl transition">
+          <div class="flex flex-row items-center gap-2">
+            <LayersIcon {..} class="size-5" />
+            <p class="text-lg">
+              "Event"
+            </p>
+          </div>
+          <p class="text-orangea-dim max-w-prose text-sm">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut auctor magna in lacus egestas tincidunt. Maecenas erat tellus, dapibus feugiat purus eget, tincidunt semper risus."
+          </p>
+        </div>
       </div>
     </Section>
   }
