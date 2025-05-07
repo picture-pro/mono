5d1bd69d617f106d145842848ee7f489d19475e1
John Lewis <github@jlewis.sh>
2025-05-07 11:42:26 +0200

    feat(base-components): add `class` for `Prose` and `Title`

--- crates/base-components/src/prose.rs
+++ crates/base-components/src/prose.rs
@@ -1,9 +1,19 @@
 use leptos::prelude::*;
 
 #[component]
-pub fn Prose(children: Children) -> impl IntoView {
+pub fn Prose(
+  children: Children,
+  /// Extra classes to add.
+  #[prop(into, optional)]
+  class: MaybeProp<String>,
+) -> impl IntoView {
+  const BASE_CLASS: &str = "text-base-dim max-w-prose";
+  let final_class = Signal::derive(move || {
+    format!("{BASE_CLASS} {}", class().unwrap_or_default())
+  });
+
   view! {
-    <p class="text-base-dim max-w-prose">
+    <p class=final_class>
       { children() }
     </p>
   }
--- crates/base-components/src/title.rs
+++ crates/base-components/src/title.rs
@@ -1,9 +1,19 @@
 use leptos::prelude::*;
 
 #[component]
-pub fn Title(children: Children) -> impl IntoView {
+pub fn Title(
+  children: Children,
+  /// Extra classes to add.
+  #[prop(into, optional)]
+  class: MaybeProp<String>,
+) -> impl IntoView {
+  const BASE_CLASS: &str = "text-6xl font-serif font-light tracking-tight mb-4";
+  let final_class = Signal::derive(move || {
+    format!("{BASE_CLASS} {}", class().unwrap_or_default())
+  });
+
   view! {
-    <p class="text-6xl font-serif font-light tracking-tight mb-4">
+    <p class=final_class>
       { children() }
     </p>
   }
