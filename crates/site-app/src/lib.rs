1cda1564e32bbe5c9843e32ab2ca042ee92c3341
John Lewis <github@jlewis.sh>
2025-05-08 15:06:40 +0200

    feat(app): add `PageCover`

--- crates/site-app/src/lib.rs
+++ crates/site-app/src/lib.rs
@@ -8,7 +8,7 @@ pub mod server_fns;
 
 use app_upload::UploadPhotoPage;
 use base_components::PageContainer;
-use leptos::prelude::*;
+use leptos::{prelude::*, server::codee::string::FromToStringCodec};
 use leptos_meta::{
   provide_meta_context, HashedStylesheet, MetaTags, Style, Title,
 };
@@ -16,6 +16,7 @@ use leptos_router::{
   components::{Route, Router, Routes},
   path,
 };
+use leptos_use::use_cookie;
 pub use models;
 
 use self::{components::Header, pages::*};
@@ -52,7 +53,8 @@ pub fn App() -> impl IntoView {
     <Title text="PicturePro"/>
 
     <Router>
-      <Header/>
+      <PageCover />
+      <Header />
       <PageContainer>
         <Routes fallback=NotFoundPage>
           <Route path=path!("/") view=HomePage />
@@ -78,4 +80,38 @@ fn protect<
   func: F,
 ) -> impl Send + Clone + 'static + Fn() -> impl IntoAny {
   move || view! { <ProtectedPage> { func() } </ProtectedPage> }
+}
+
+#[island]
+fn PageCover() -> impl IntoView {
+  let (hide, set_hide) = use_cookie::<bool, FromToStringCodec>("hide_loader");
+  let hide = Signal::derive(move || hide.get().is_some_and(|v| v));
+
+  // set hide right after render
+  Effect::watch(
+    move || (),
+    move |_, _, _| {
+      set_hide.set(Some(true));
+    },
+    true,
+  );
+
+  let class = Signal::derive(move || {
+    let mut class = "absolute inset-0 bg-base-app flex flex-col items-center \
+                     justify-center gap-4 pointer-events-none"
+      .to_string();
+    if hide() {
+      class.push_str(
+        " delay-[500ms] duration-[500ms] ease-in-out opacity-0 \
+         transition-opacity",
+      );
+    }
+    class
+  });
+
+  view! {
+    <div class=class>
+      <base_components::Title>"PicturePro"</base_components::Title>
+    </div>
+  }
 }
