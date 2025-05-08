5b671c9f6dcb30c5b51cf491d3d4b7b5cf4bbd08
John Lewis <github@jlewis.sh>
2025-05-08 14:27:20 +0200

    feat(app): further adjustments to `ColoredBox`

--- crates/site-app/src/pages/home_page.rs
+++ crates/site-app/src/pages/home_page.rs
@@ -17,34 +17,34 @@ pub fn HomePage() -> impl IntoView {
     </Section>
 
     <Section>
-      <div class="grid grid-cols-2 w-full gap-4">
+      <div class="grid grid-cols-2 w-full gap-3 xs:gap-4">
         <ColoredBox
           href="/upload-photo"
-          extra_outer_class="bg-ruby-3 dark:bg-rubydark-3 border border-ruby-normal text-rubya-normal"
+          extra_outer_class="bg-ruby-3 dark:bg-rubydark-3 border-ruby-normal text-rubya-normal"
           extra_dim_text_class="text-rubya-dim"
           title="Single Subject"
-          icon_view={ view! { <CameraIcon {..} class="size-5" /> }.into_any() }
+          icon_view={ view! { <CameraIcon {..} class="size-5 shrink-0" /> }.into_any() }
         />
 
         <ColoredBox
-          extra_outer_class="bg-jade-3 dark:bg-jadedark-3 border border-jade-normal text-jadea-normal"
+          extra_outer_class="bg-jade-3 dark:bg-jadedark-3 border-jade-normal text-jadea-normal"
           extra_dim_text_class="text-jadea-dim"
           title="Upload to Marketplace"
-          icon_view={ view! { <UploadIcon {..} class="size-5" /> }.into_any() }
+          icon_view={ view! { <UploadIcon {..} class="size-5 shrink-0" /> }.into_any() }
         />
 
         <ColoredBox
-          extra_outer_class="bg-cyan-3 dark:bg-cyandark-3 border border-cyan-normal text-cyana-normal"
+          extra_outer_class="bg-cyan-3 dark:bg-cyandark-3 border-cyan-normal text-cyana-normal"
           extra_dim_text_class="text-cyana-dim"
           title="Explore Marketplace"
-          icon_view={ view! { <MagnifyingGlassIcon {..} class="size-5" /> }.into_any() }
+          icon_view={ view! { <MagnifyingGlassIcon {..} class="size-5 shrink-0" /> }.into_any() }
         />
 
         <ColoredBox
-          extra_outer_class="bg-orange-3 dark:bg-orangedark-3 border border-orange-normal text-orangea-normal"
+          extra_outer_class="bg-orange-3 dark:bg-orangedark-3 border-orange-normal text-orangea-normal"
           extra_dim_text_class="text-orangea-dim"
           title="Event"
-          icon_view={ view! { <LayersIcon {..} class="size-5" /> }.into_any() }
+          icon_view={ view! { <LayersIcon {..} class="size-5 shrink-0" /> }.into_any() }
         />
       </div>
     </Section>
@@ -60,7 +60,7 @@ fn ColoredBox(
   icon_view: AnyView,
 ) -> impl IntoView {
   let outer_class = format!(
-    "flex flex-col gap-2 p-4 rounded-xl shadow-xl transition \
+    "flex flex-col gap-2 p-3 xs:p-4 border rounded-xl shadow-xl transition \
      {extra_outer_class}"
   );
   let dim_text_class = format!("max-w-prose text-sm {extra_dim_text_class}");
@@ -69,7 +69,7 @@ fn ColoredBox(
     <a href=href class=outer_class>
       <div class="flex flex-row items-center gap-2">
         { icon_view }
-        <p class="sm:text-lg">
+        <p class="xs:text-lg leading-tight">
           { title }
         </p>
       </div>
