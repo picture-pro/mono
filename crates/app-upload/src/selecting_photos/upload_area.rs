28060de0af47ff8f858cccf0093abcd5262362df
John Lewis <github@jlewis.sh>
2025-05-29 17:57:12 +0200

    feat(upload): add single-upload mode

--- crates/app-upload/src/selecting_photos/upload_area.rs
+++ crates/app-upload/src/selecting_photos/upload_area.rs
@@ -8,17 +8,27 @@ use crate::UploadStateStoreFields;
 
 #[component]
 pub(super) fn UploadArea() -> impl IntoView {
-  use lsc::icons::*;
-
   let class = "w-full bg-base-2 dark:bg-basedark-2 text-base-dim border-2 \
                border-dashed border-base-8 dark:border-basedark-8 rounded-xl \
-               flex flex-row items-stretch";
-
-  let icon_class = "size-24 text-basea-11 dark:text-basedarka-11";
+               flex flex-row items-stretch relative";
 
   let context: Store<super::super::UploadState> = expect_context();
 
+  let disabled = Signal::derive(move || {
+    let state = context
+      .selecting_photos_0()
+      .expect("`UploadContext` not in state `SelectingPhotos`");
+    #[cfg(feature = "single-photo-upload")]
+    return !state.photos().read().is_empty();
+    #[cfg(not(feature = "single-photo-upload"))]
+    false
+  });
+
   let handler = move |ev: Event| {
+    if disabled() {
+      return;
+    }
+
     let state = context
       .selecting_photos_0()
       .expect("`UploadContext` not in state `SelectingPhotos`");
@@ -43,41 +53,90 @@ pub(super) fn UploadArea() -> impl IntoView {
 
   view! {
     <div class=class>
-
-      <label
-        class="flex-1 flex flex-row justify-center items-center p-8 cursor-pointer"
-        for="camera-input"
-      >
-        <div class="flex flex-col items-center gap-4">
-          <CameraIcon {..} class=icon_class />
-          <div class="flex flex-col items-center gap-1 text-base-dim text-sm">
-            <p>"Take a photo with your camera."</p>
-            <input
-              type="file" class="hidden" id="camera-input" accept="image/*"
-              capture="environment" multiple="multiple" on:change=handler
-            />
-          </div>
-        </div>
-      </label>
-
-      <div class="my-4 w-[1px] border-l-2 border-dashed border-base-8 dark:border-basedark-8" />
-
-      <label
-        class="flex-1 flex flex-row justify-center items-center p-8 cursor-pointer"
-        for="file-input"
-      >
-        <div class="flex flex-col items-center gap-4">
-          <UploadIcon {..} class=icon_class />
-          <div class="flex flex-col items-center gap-1 text-base-dim text-sm">
-            <p>"Upload a photo from your device."</p>
-            <input
-              type="file" class="hidden" id="file-input" accept="image/*"
-              multiple="multiple" on:change=handler
-            />
-          </div>
-        </div>
-      </label>
-
+      <UploadAreaInner handler=handler disabled=disabled />
+      { move || {
+        disabled().then_some(view! { <UploadAreaOverlay /> })
+      }}
     </div>
+  }
+}
+
+#[component]
+fn UploadAreaOverlay() -> impl IntoView {
+  let class = "absolute inset-0 bg-base-2 dark:bg-basedark-2 text-base-dim \
+               flex flex-col justify-center items-center";
+  view! {
+    <div class=class>
+      <p class="text-2xl">"Photo Already Selected"</p>
+    </div>
+  }
+}
+
+#[component]
+fn UploadAreaInner(
+  handler: impl Fn(Event) + Copy + 'static,
+  disabled: Signal<bool>,
+) -> impl IntoView {
+  use lsc::icons::*;
+
+  let icon_class = "size-24 text-basea-11 dark:text-basedarka-11";
+
+  #[cfg(feature = "single-photo-upload")]
+  let camera_input_el = view! {
+    <input
+      type="file" class="hidden" id="camera-input" accept="image/*"
+      capture="environment" on:change=handler disabled=disabled
+    />
+  };
+  #[cfg(not(feature = "single-photo-upload"))]
+  let camera_input_el = view! {
+    <input
+      type="file" class="hidden" id="camera-input" accept="image/*"
+      capture="environment" multiple="multiple" on:change=handler disabled=disabled
+    />
+  };
+  #[cfg(feature = "single-photo-upload")]
+  let file_input_el = view! {
+    <input
+      type="file" class="hidden" id="file-input" accept="image/*"
+      on:change=handler disabled=disabled
+    />
+  };
+  #[cfg(not(feature = "single-photo-upload"))]
+  let file_input_el = view! {
+    <input
+      type="file" class="hidden" id="file-input" accept="image/*"
+      multiple="multiple" on:change=handler disabled=disabled
+    />
+  };
+
+  view! {
+    <label
+      class="flex-1 flex flex-row justify-center items-center p-8 cursor-pointer"
+      for="camera-input"
+    >
+      <div class="flex flex-col items-center gap-4">
+        <CameraIcon {..} class=icon_class />
+        <div class="flex flex-col items-center gap-1 text-base-dim text-sm">
+          <p>"Take a photo with your camera."</p>
+          { camera_input_el }
+        </div>
+      </div>
+    </label>
+
+    <div class="my-4 w-[1px] border-l-2 border-dashed border-base-8 dark:border-basedark-8" />
+
+    <label
+      class="flex-1 flex flex-row justify-center items-center p-8 cursor-pointer"
+      for="file-input"
+    >
+      <div class="flex flex-col items-center gap-4">
+        <UploadIcon {..} class=icon_class />
+        <div class="flex flex-col items-center gap-1 text-base-dim text-sm">
+          <p>"Upload a photo from your device."</p>
+          { file_input_el }
+        </div>
+      </div>
+    </label>
   }
 }
